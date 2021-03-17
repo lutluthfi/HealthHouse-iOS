06301e6ebac2440dd1b291d7fa0a4321aebec499
Arif Luthfiansyah <lutluthfi@icloud.com>
2021-03-17 14:19:13 +0700

    [REMOVE] Pods directory

--- Pods/Target Support Files/Pods-HealthDiary-HealthDiaryUITests/Pods-HealthDiary-HealthDiaryUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HealthDiary_HealthDiaryUITests : NSObject
-@end
-@implementation PodsDummy_Pods_HealthDiary_HealthDiaryUITests
-@end
--- Pods/Target Support Files/Pods-HealthDiary/Pods-HealthDiary-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HealthDiary : NSObject
-@end
-@implementation PodsDummy_Pods_HealthDiary
-@end
